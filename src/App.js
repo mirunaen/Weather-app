bff50a1db7ce0262e37db9a0c02c8da344cc64b5
mirunaen <mirunaen@gmail.com>
2020-05-22 21:52:41 +0200

    Api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import "weather-icons/css/weather-icons.css";
 import Weather from "../src/app_component/weather.component";
 
+const API = "aae9fd5817293a02b244f3ba2b6456ca";
 function App() {
   return (
     <div className="App">
