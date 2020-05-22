8f395b30f0f3ebb593ca8d836ffb7cbe6c68648d
mirunaen <mirunaen@gmail.com>
2020-05-22 21:40:29 +0200

    added smt

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -5,11 +5,25 @@ const Weather = () => {
     <div className="container">
       <div className="cards">
         <h1>Spain</h1>
-        <h5 className="py-4"></h5>
-        <i className="wi wi-day-sunny"></i>
+        <h5 className="py-4">
+          <i className="wi wi-day-sunny display-1"></i>
+        </h5>
+        <h1 className="py-2">25&deg;</h1>
+        {/**show max and min temp */}
+        {minmaxTemp(24, 19)}
+
+        <h4 className="py-3">Slow rain</h4>
       </div>
     </div>
   );
 };
 
+function minmaxTemp(min, max) {
+  return (
+    <h3>
+      <span className="px-4">{min}&deg; </span>
+      <span className="px-4">{max}&deg; </span>
+    </h3>
+  );
+}
 export default Weather;
