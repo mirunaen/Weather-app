1d966980108c8b3296c5ae75035f93a063e6f76c
mirunaen <mirunaen@gmail.com>
2020-05-23 19:21:08 +0200

    fetch description

--- src/App.js
+++ src/App.js
@@ -38,9 +38,9 @@ class App extends React.Component {
       city: response.name,
       country: response.sys.country,
       celsius: this.calCelsius(response.main.temp),
-      temp_max: response.main.temp_max,
-      temp_min: response.main.temp_min,
-      //description: response.Weather[0].description,
+      temp_max: this.calCelsius(response.main.temp_max),
+      temp_min: this.calCelsius(response.main.temp_min),
+      description: response.weather[0].description,
     });
   };
   render() {
