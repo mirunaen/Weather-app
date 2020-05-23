adf74520d7e30f36385df0b66f39d848b8abbf6a
mirunaen <mirunaen@gmail.com>
2020-05-23 19:38:21 +0200

    added new weatherIcons

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ class App extends React.Component {
     this.getWeather();
     this.weatherIcon = {
       Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
     };
   }
 
