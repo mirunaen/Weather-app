8bcf5d3e1d20e1e08523dc693f2f26d60761c0ec
mirunaen <mirunaen@gmail.com>
2020-05-23 19:07:11 +0200

    using props

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -10,11 +10,11 @@ const Weather = (props) => {
         <h5 className="py-4">
           <i className="wi wi-day-sunny display-1"></i>
         </h5>
-        <h1 className="py-2">25&deg;</h1>
+        <h1 className="py-2">{props.temp_celsius}&deg;</h1>
         {/**show max and min temp */}
-        {minmaxTemp(24, 19)}
+        {minmaxTemp(props.temp_min, props.temp_max)}
 
-        <h4 className="py-3">Slow rain</h4>
+        <h4 className="py-3">{props.description}</h4>
       </div>
     </div>
   );
