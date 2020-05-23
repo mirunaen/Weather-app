4c259584d4a1cb204de6136609611fb5a7724b98
mirunaen <mirunaen@gmail.com>
2020-05-23 20:29:08 +0200

    set style

--- src/app_component/form.component.jsx
+++ src/app_component/form.component.jsx
@@ -1,10 +1,10 @@
-import React from "react;";
+import React from "react";
 
 const Form = (props) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-3 offset-md-2">
           <input
             type="text"
             className="form-control"
@@ -21,7 +21,7 @@ const Form = (props) => {
             autoComplete="off"
           />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-3 mt-md-0 text-md-left">
           <button className="btn btn-warning">Get Weather</button>
         </div>
       </div>
