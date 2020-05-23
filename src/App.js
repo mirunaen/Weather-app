b524913a550fc68b3d23fdd7095fb26f4ae7a2bc
mirunaen <mirunaen@gmail.com>
2020-05-23 20:51:26 +0200

    error messague

--- src/App.js
+++ src/App.js
@@ -71,21 +71,25 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
-    );
-    const response = await api_call.json();
-    console.log(response);
+    if (city && country) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      );
+      const response = await api_call.json();
+      console.log(response);
 
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: this.calCelsius(response.main.temp),
-      temp_max: this.calCelsius(response.main.temp_max),
-      temp_min: this.calCelsius(response.main.temp_min),
-      description: response.weather[0].description,
-    });
-    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+      this.setState({
+        city: response.name,
+        country: response.sys.country,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
   };
   render() {
     return (
